4f8c5bc500abf826e0e4f1100a41a1d9bfa3b8d4
Manuel Rivera <menu.rivera@gmail.com>
2023-07-05 15:35:10 -0600

    [ADD] -> basic promptInput.tsx functionality, main page mods

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import PromptInput from "@/components/PromptInput";
 
 export default function Home() {
 	return (
-		<>
+		<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
 			<h1>AI website builder</h1>
 			<PromptInput />
-		</>
+		</div>
 	)
 }
