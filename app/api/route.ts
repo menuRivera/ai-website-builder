b4ce479e9cabb3281bf98f7219a31e246b71c395
Manuel Rivera <menu.rivera@gmail.com>
2023-07-05 21:10:49 -0600

    [ADD] -> GET /api route created with incomplete functionality

---
+++ app/api/route.ts
@@ -0,0 +1,43 @@
+import { exec } from "child_process";
+import { NextResponse } from "next/server";
+import path from "path";
+// import * as fs from 'node:fs/promises'
+
+// __dirname is equal to ai-website-builder/.next/server/app/api
+// route: /api
+
+// The route and method are irrelevant right now because of the
+// demostrative nature of the project. What is important though, is 
+// the fact that it is being executed on the server side.
+
+export async function GET(req: Request) {
+	// const dirPath = path.join(__dirname, '/testDir')
+	// fs.mkdirSync(dirPath)
+
+	// Verify dependencies
+	console.log(await execute(`sh scripts/verify-dependencies.sh`))
+
+	// Create the base project
+	// clone https://github.com/menuRivera/base-nextjs-template	
+	// console.log(await execute(`sh scripts/clone-base-project.sh`))
+	console.log(await execute('git clone https://github.com/menuRivera/base-nextjs-template'))
+
+	// Contextualize the ai about the structure and dependencies of the base project
+
+	// Ask it the generate the required components and pages for the website in an 
+	// array of the form [{title, description, route}]
+
+	// Make the necesary modifications to the base project using predefined
+	// bash scripts and the information from the ai
+	return NextResponse.json({ success: true })
+}
+
+const execute = (command: string) => {
+	// function for executing shell commands with ease
+	return new Promise((resolve, reject) => {
+		exec(command, (error, stdout, stderr) => {
+			if (!error) return resolve(stdout)
+			reject(error || stderr)
+		})
+	})
+}
---
+++ scripts/verify-dependencies.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/bash
+
+echo 'VERIFY DEPENDENCIES SCRIPT'
