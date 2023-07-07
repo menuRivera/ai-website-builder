8a8091c61b17272a877e09370c372de8cdaad862
Manuel Rivera <menu.rivera@gmail.com>
2023-07-07 16:42:56 -0600

    [MOD] -> curly brackets problem

--- app/api/_lib/prompts.ts
+++ app/api/_lib/prompts.ts
@@ -51,13 +51,13 @@ export const componentPrompt = new PromptTemplate({
 	description: The home page for the university website, contains information of the campus, contact information and links to the other pages, contains a landing page, followed by some photos of the campus, reviews of previous students and a contact us segment
 
 	NEXTJS COMPONENT:
-	import { Box, Button, Typography } from "@mui/material";
+	import {{ Box, Button, Typography }} from "@mui/material";
 	import Link from "next/link";
 
-	export default function Home() {
+	export default function Home() {{
 		return (
-			<Box sx={{ textAlign: 'center' }}>
-				<Box sx={{ height: '100vh', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+			<Box sx={{{{ textAlign: 'center' }}}}>
+				<Box sx={{{{ height: '100vh', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}}}>
 					<Box>
 						<Typography variant="h2" component="h1" gutterBottom>
 							University of Colorado
@@ -65,22 +65,22 @@ export const componentPrompt = new PromptTemplate({
 						<Typography variant="body1" component="p">
 							Empowering Minds, Igniting Potential
 						</Typography>
-						<Button sx={{marginTop: '20px'}} variant="contained"><Link href='/careers'>Explore careers</Link></Button>
+						<Button sx={{{{marginTop: '20px'}}}} variant="contained"><Link href='/careers'>Explore careers</Link></Button>
 					</Box>
 				</Box>
 
-				<Box sx={{ backgroundColor: '#f0f0f0', padding: '50px' }}>
+				<Box sx={{{{ backgroundColor: '#f0f0f0', padding: '50px' }}}}>
 					<Typography variant="h4" component="h2" gutterBottom>
 						Our Campus
 					</Typography>
-					<Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}>
+					<Box sx={{{{ display: 'flex', justifyContent: 'center', marginTop: '30px' }}}}>
 						<img src="campus1.png" alt="Campus 1" width="300" height="200" />
 						<img src="campus2.png" alt="Campus 2" width="300" height="200" />
 						<img src="campus3.png" alt="Campus 3" width="300" height="200" />
 					</Box>
 				</Box>
 
-				<Box sx={{ marginTop: '100px' }}>
+				<Box sx={{{{ marginTop: '100px' }}}}>
 					<Typography variant="h4" component="h2" gutterBottom>
 						Students opinions
 					</Typography>
@@ -95,7 +95,7 @@ export const componentPrompt = new PromptTemplate({
 					</Typography>
 				</Box>
 
-				<Box sx={{ marginTop: '100px' }}>
+				<Box sx={{{{ marginTop: '100px' }}}}>
 					<Typography variant="h4" component="h2" gutterBottom>
 						Contact
 					</Typography>
@@ -111,7 +111,7 @@ export const componentPrompt = new PromptTemplate({
 				</Box>
 			</Box>
 		)
-	}
+	}}
 
 	WEBSITE INFORMATION:
 	description: {userPrompt}
